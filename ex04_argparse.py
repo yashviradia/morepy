23a0a5fe47e4a91f3fae322eefa6324b0ea16f0f
yashviradiaofficial <yashviradiaofficial@gmail.com>
2020-08-20 20:00:03 +0530

    again done the argparse correctly.

--- ex04_argparse.py
+++ ex04_argparse.py
@@ -1,36 +1,16 @@
-"""
-from sys import argv
-
-scripts, first, second = argv
-
-print(f"this is script 1 {script}.")
-print(f"this is script 2 {first}.")
-print(f"this is script 3 {second}.")
-"""
-"""
-import argparse
-
-parser = argparse.ArgumentParser(description='Process some integers.')
-parser.add_argument('integers', metavar='N', type=int, nargs='+',
-                    help='an integer for the accumulator')
-parser.add_argument('--sum', dest='accumulate', action='store_const',
-                    const=sum, default=max,
-                    help='sum the integers (default: find the max)')
-
-args = parser.parse_args()
-print(args.accumulate(args.integers))
-"""
 
 import argparse
 parser = argparse.ArgumentParser()
-
 parser.add_argument('integers', metavar='N', type=int, nargs='+')
-parser.add_argument('-f', '--foo', help='foo help')
-parser.add_argument('-b', '--bar', help='bar help')
-parser.add_argument('-z', '--baz', help='baz help')
-parser.add_argument('-t', '--turn-on', action='store_true')
-parser.add_argument('-x', '--exclude', action='store_false')
-parser.add_argument('-s', '--start', action='store_true')
-args = parser.parse_args()
+parser.add_argument('--sum', dest='accumulate', action='store_const', const=sum, default=max,
+                    help='sum the integers (default: find the max)')
+parser.add_argument('echo', help="echo the string you use here.")
+parser.add_argument('-z', '--start', action='store_true')
+parser.add_argument('-x', '--stop', action='store_false')
+parser.add_argument('-f', '--foo', help="foo help")
+parser.add_argument('-b', '--bar', help="bar help")
+parser.add_argument('-c', '--char', help="char help")
+parser.add_argument("square", help="display a square of a given number.", type=int)
 
+args = parser.parse_args()
 print(args)
